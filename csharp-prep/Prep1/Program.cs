aa1b9e120a0acf46b3cb36ce3e2141317335def6
Tristan Cheney <12345678+AnAquaticDerg@users.noreply.github.com>
2026-01-10 23:48:29 -0500

    Completed steps one & 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // First Step
+        Console.WriteLine("What is your first name?");
+        string first_name = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string last_name = Console.ReadLine();
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+
+        // Second Step
+        Console.WriteLine("What is your grade percentage?");
+        string gradeString = Console.ReadLine();
+        int grade = int.Parse(gradeString);
+
+        string letterGrade = "";
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"{letterGrade}");
+
+        if (letterGrade == "A" || letterGrade == "B" || letterGrade == "C")
+        {
+            Console.WriteLine("Congradulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass, but if you work hard, you'll pass next time!");
+        }
     }
 }
