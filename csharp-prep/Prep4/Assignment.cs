33cf52e4530e18390ee3f6188117ba21bf23e21c
Tristan <tristan.cheney@icloud.com>
2026-01-27 19:12:07 -0500

    Completed the prep activities

---
+++ csharp-prep/Prep4/Assignment.cs
@@ -0,0 +1,23 @@
+public class Assignment
+{
+    private string _name;
+    private string _topic;
+
+    public Assignment()
+    {
+        _name = "";
+        _topic = "";
+    }
+    public Assignment(string name, string topic)
+    {
+        _name = name;
+        _topic = topic;
+    }
+
+
+    public string GetSummary()
+    {
+        return $"{_name} - {_topic}";
+    }
+
+}
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.Clear();
+
+        MathAssignment mathAssignment = new MathAssignment("Tristan", "Decimals", "Section 5", "1-5, 10-23");
+        Console.WriteLine($"{mathAssignment.GetHomeworkList()}\n");
+
+        WritingAssignment writingAssignment = new WritingAssignment("Tristan", "English wiritng skilsksj", "Coolio Toolio");
+        Console.WriteLine(writingAssignment.GetWritingAssignment());
     }
 }
